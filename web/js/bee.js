b37596c11246c19e4287ba6ddd7a761b68a7ceca
Javier Durante <javier.durante@findly.com>
2014-12-18 09:32:50 +1300

    test assets and the bee player

--- web/js/bee.js
+++ web/js/bee.js
@@ -1,17 +1,19 @@
-var Bee = {
+var Bee = function(game) {
     var playerLayer = game.createLayer("players");
     var player = new PixelJS.Player();
     player.addToLayer(playerLayer);
     player.pos = { x: 200, y: 300 };
-    player.size = { width: 32, height: 32 };
+    player.size = { width: 64, height: 64 };
     player.velocity = { x: 100, y: 100 };
     player.asset = new PixelJS.AnimatedSprite();
     player.asset.prepare({ 
-        name: 'char.png', 
+        name: 'blackbox.png',
         frames: 3, 
-        rows: 4,
-        speed: 100,
+        rows: 3,
+        speed: 150,
         defaultFrame: 1
     });
 }
+
+    
     
