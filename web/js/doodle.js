3756eb48acf0dfce2768c2352b160bff0e8c4835
Javier Durante <javier.durante@findly.com>
2014-12-18 09:32:50 +1300

    test assets and the bee player

--- web/js/doodle.js
+++ web/js/doodle.js
@@ -1,22 +1,29 @@
-var Doodle = {
-    initGame: function() {
-        if (document.readyState == "complete") {
-            var game = new PixelJS.Engine();
-            game.init({
-                container: 'game_container',
-                width: 800,
-                height: 600
-            });
-            
-            game.run(function (elapsedTime, dt) {
-            });
-        }
-    },
-    initPlayer: function() {
-        
-    }
+function debug(m){
+    console.log(m);
+}
+
+var Doodle = function() {
+    this.initGame();
+}
+
+Doodle.prototype.initGame = function() {
+    debug('init');
+    this.game = new PixelJS.Engine();
+    this.game.init({
+        container: 'game_container',
+        width: 800,
+        height: 600
+    });
+    this.beePlayer = new Bee(this.game);    
+}
+
+Doodle.prototype.run = function() {
+    this.game.loadAndRun(function (elapsedTime, dt) {});
 }
 
 document.onreadystatechange = function () {
-    Doodle.initGame();   
+    if (document.readyState == "complete") {
+        var doodle = new Doodle();
+        doodle.run();
+    }
 }
