89a40b9856cdd4ef42dcafa4ef87801d30b711de
Javier Durante <javier.durante@findly.com>
2014-12-18 20:35:26 +1300

    fixed heroku port

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ var app = express();
 
 app.use(express.static('.'));
 
-var port = parseInt(process.env.OPENSHIFT_NODEJS_PORT) || 8000;
-var ipaddr = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = parseInt(process.env.PORT) || 8000;
 
-var server = app.listen(port, ipaddr, function() {
+var server = app.listen(port, function() {
 	console.log('Listening on port %d', port);
 });
