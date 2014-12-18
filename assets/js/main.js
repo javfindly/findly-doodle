648aa69afa5f470fffd925877cec040643e262c1
Javier Durante <javier.durante@findly.com>
2014-12-19 11:30:57 +1300

    added game lost message and social share

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,13 @@ var _ = require('lodash');
 var $ = require('jquery');
 var Doodle = require('./doodle.js');
 var PixelJS = require('./vendors/pixel.js');
+var MainPage = require('./page.js');
 
 PixelJS.assetPath = "assets";
 
 $(document).ready(function () {
   window.doodle = new Doodle();
   window.doodle.start();
+  window.mainPage = new MainPage();
+  window.mainPage.start();
 });
