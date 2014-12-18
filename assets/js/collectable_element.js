4e988d9042afcdceb18f6f6c1670799800f01326
solediez <sole@findly.com>
2014-12-18 14:54:30 +1300

    commit to check

--- assets/js/collectable_element.js
+++ assets/js/collectable_element.js
@@ -6,10 +6,9 @@ var CONSTANTS = {
   TYPE: 'candidate'
 }
 
-var CollectableElement = function (game) {
-  this.layer = game.createLayer("collectables");
-  this.collectableEntity = this.layer.createEntity();
-  this.collectableEntity.pos = { x: 400, y: 0 };
+var CollectableElement = function (layer) {
+  this.collectableEntity = layer.createEntity();
+  this.collectableEntity.pos = { x: Math.floor(Math.random() * 600) + 1, y: 0 };
   this.collectableEntity.size = { width: 12, height: 12 };
   this.collectableEntity.velocity = { x: 200, y: 100 };
   this.collectableEntity.asset = new PixelJS.Sprite();
@@ -22,7 +21,8 @@ var CollectableElement = function (game) {
 
 CollectableElement.prototype.CONSTANTS = CONSTANTS;
 
-CollectableElement.prototype.drop = function() {
+CollectableElement.prototype.drop = function(velocity) {
+  // this.collectableEntity.velocity = velocity;
   this.collectableEntity.moveDown();
 };
 
